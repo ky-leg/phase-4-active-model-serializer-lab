ffc83b6d6d6dc314e30080e3a8ef16b05b5289a4
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-05-05 23:20:53 -0400

    commit tree ecc2c1ef1e7731df4950cf623c596fa67397a879

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,8 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :title, :content
+
+  belongs_to :author
+  # , serializer: PostAuthorSerializer
+  has_many :tags
+  # , serializer: PostTagsSerializer
+end
